6251c9fcf3399fde57b648ba89fb0385c5742f04
daniel <daniel@htsys.sk>
2019-12-01 00:48:10 +0100

    GMF

--- src/models/model.py
+++ src/models/model.py
@@ -1,60 +1,65 @@
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import initializers
-from tensorflow.keras.layers import Embedding, Input, Dense, Flatten, concatenate, Dropout
+from tensorflow.keras.layers import Embedding, Input, Dense, Flatten, concatenate, Dropout, dot, Lambda, Reshape, Dot
+from tensorflow.keras.backend import transpose
 import numpy as np
 
 class JokeRecommender(keras.Model):
     
-    def __init__(self, input_vector, units1 = 32, units2 = 16, units3 = 8):
+    def __init__(self, n_users, n_jokes, units1 = 100, units2 = 100, units3 = 8, dropout_rate=0.2):
         super(JokeRecommender, self).__init__()
-        
-        # # Input layers
-        # user_input = Input(shape=(1,), dtype='int32')
-        # joke_input = Input(shape=(1,), dtype='int32')
                 
-        # Embedding layers
-        self.embedding = Embedding(
-            input_dim=len(input_vector), 
-            output_dim=int(units1 / 2),
-            input_length=len(input_vector[0])
-        )
-        self.flatten = Flatten()
+        self.n_users = n_users        
+        self.n_jokes = n_jokes
 
-        # self.user_embedding = Embedding(
-        #     input_dim=len(user_ids), 
-        #     output_dim=int(units1 / 2),
-        #     input_length=1
-        # )
-        
-        # self.joke_embedding = Embedding(
-        #     input_dim=len(joke_ids),
-        #     output_dim=int(units1 / 2),
-        #     input_length=1
-        # )
-        
-        # # Concatenate user and joke embeddings
-        # self.user_flatten = Flatten()
-        # self.joke_flatten = Flatten()
+        self.split_user = Lambda(lambda x: x[:, :n_users])
+        self.split_joke = Lambda(lambda x: x[:, n_users:])
+        # Embedding layers
+        self.joke_embedding = Embedding(
+            n_jokes, 
+            n_users,
+            # input_length=len(movies[0])
+        )
+        self.joke_flatten = Flatten()
+        self.joke_dropout = Dropout(dropout_rate)
+
+        # Embedding layers
+        self.user_embedding = Embedding(
+            n_users, 
+            n_jokes,
+            # input_length=len(input_vector[0])
+        )
+        self.user_flatten = Flatten()
+        self.user_dropout = Dropout(dropout_rate)
+
+
+        # self.user_transpose = Reshape((-1, -1))
+        # self.joke_transpose = Reshape((-1, -1))
+        self.merge_dot = Dot(axes=-1)
         
         self.layer_1 = Dense(units=units1, activation='relu')
         self.layer_2 = Dense(units=units2, activation='relu')
-        self.layer_3 = Dense(units=units3, activation='relu')
-
-        self.dropout = Dropout(0.5)
+        # # self.layer_3 = Dense(units=units3, activation='relu')
+        # # self.dropout = Dropout(0.5)
         
-        self.my_output = Dense(units=10, activation='tanh')
+        self.my_output = Dense(units=1, activation='tanh')
         
 
     def call(self, x):
-        # user = self.user_embedding(x[0])
-        # joke = self.joke_embedding(x[1])
-        # x = concatenate([user, joke])
-        x = self.embedding(x)
-        x = self.flatten(x)
+        # print(x.value_index())
+        user = self.split_user(x)
+        user = self.user_dropout(self.user_flatten(self.user_embedding(user)))
+        
+        joke = self.split_joke(x)
+        joke = self.joke_dropout(self.joke_flatten(self.joke_embedding(joke)))
+
+        # print(user, transpose(user))
+        # x = dot([user, joke], axes=-1)
+        x = self.merge_dot([user, joke])
+
         x = self.layer_1(x)
         x = self.layer_2(x)
-        x = self.layer_3(x)
-        x = self.dropout(x)
+
         x = self.my_output(x)
         return x
---
+++ src/models/model_old.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import initializers
+from tensorflow.keras.layers import Embedding, Input, Dense, Flatten, concatenate, Dropout
+import numpy as np
+
+class JokeRecommender(keras.Model):
+    
+    def __init__(self, input_vector, units1 = 32, units2 = 16, units3 = 8):
+        super(JokeRecommender, self).__init__()
+        
+        # # Input layers
+        # user_input = Input(shape=(1,), dtype='int32')
+        # joke_input = Input(shape=(1,), dtype='int32')
+                
+        # Embedding layers
+        self.embedding = Embedding(
+            input_dim=len(input_vector), 
+            output_dim=int(units1 / 2),
+            input_length=len(input_vector[0])
+        )
+        self.flatten = Flatten()
+
+        # self.user_embedding = Embedding(
+        #     input_dim=len(user_ids), 
+        #     output_dim=int(units1 / 2),
+        #     input_length=1
+        # )
+        
+        # self.joke_embedding = Embedding(
+        #     input_dim=len(joke_ids),
+        #     output_dim=int(units1 / 2),
+        #     input_length=1
+        # )
+        
+        # # Concatenate user and joke embeddings
+        # self.user_flatten = Flatten()
+        # self.joke_flatten = Flatten()
+        
+        self.layer_1 = Dense(units=units1, activation='relu')
+        self.layer_2 = Dense(units=units2, activation='relu')
+        self.layer_3 = Dense(units=units3, activation='relu')
+
+        self.dropout = Dropout(0.5)
+        
+        self.my_output = Dense(units=1, activation='sigmoid')
+        
+
+    def call(self, x):
+        # user = self.user_embedding(x[0])
+        # joke = self.joke_embedding(x[1])
+        # x = concatenate([user, joke])
+        x = self.embedding(x)
+        x = self.flatten(x)
+        x = self.layer_1(x)
+        x = self.layer_2(x)
+        x = self.layer_3(x)
+        x = self.dropout(x)
+        x = self.my_output(x)
+        return x
