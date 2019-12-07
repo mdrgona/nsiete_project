3a4b2f1df6b67c0223fe0773b062b886bb1ae2d9
mdrgona <xdrgonam@stuba.sk>
2019-12-08 00:14:59 +0100

    Split dataset only one time

---
+++ src/data/split_data.py
@@ -0,0 +1,12 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+df = pd.read_csv('../../data/Jester-Dataset-ratings.csv', delimiter=',')
+
+train, test = train_test_split(df, test_size=0.2, random_state=1)
+
+print("Train samples: " + str(len(train)))
+print("Test samples: " + str(len(test)))
+
+train.to_csv('../../data/Jester-train')
+test.to_csv('../../data/Jester-test')
